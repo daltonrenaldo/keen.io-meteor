86658b70324e21be71f35ddb6a15ecb360492870
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2014-07-20 18:54:50 -0400

    intialize commit

---
+++ keen.js
@@ -0,0 +1,1 @@
+var Keen = require('keen.io');
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "Meteor wrapper of Keen IO: an online service to collect, analyze, and visualize your data."
+});
+
+Npm.depends({"keen.io" : "~0.1.2"})
+
+Package.on_use(function (api, where) {
+  api.add_files('keen.js', ['client', 'server']);
+  if (typeof api.export !== 'undefined') {
+    api.export(['Keen'], ['client', 'server']);
+  }
+});
